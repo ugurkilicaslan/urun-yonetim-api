9a0bbb0dbb86461cd0e56fe2d94de35f515018ec
ugurkilicaslan <ugurkilicaslan@ogr.bandirma.edu.tr>
2025-11-25 15:11:30 +0300

    Fix server.js for Render deployment

--- OneDrive/Masaüstü/stilera-backend/server.js
+++ OneDrive/Masaüstü/stilera-backend/server.js
@@ -4,26 +4,24 @@ const cors = require("cors");
 
 dotenv.config();
 
+const connectDB = require("./src/config/db");
 const productRoutes = require("./src/routes/productRoutes");
 const authRoutes = require("./src/routes/authRoutes");
 const cartRoutes = require("./src/routes/cartRoutes");
-const connectDB = require("./src/config/db");
 
-const app = express();
 connectDB();
+const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
 
 app.get("/", (req, res) => {
-  res.send("API Çalışıyor");
+  res.send("Backend Çalışıyor Render Üzerinde!");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log("SERVER RUNNING ON PORT " + PORT));
